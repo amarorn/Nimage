6b9dea3571cc1b70b70a2eeddd1b910c6c88d90e
amaro <j.amarorn@gmail.com>
2025-02-19 17:57:24 -0300

    Add health check endpoint and update MongoDB URI configuration

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ import metaRoutes from "./interfaces/routes/metaRoutes";
 
 const app = express();
 app.use(express.json());
+
+// Adicionando o endpoint de health check
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "UP", message: "Service is running smoothly" });
+});
+
 app.use("/api", vendedorRoutes);
 app.use("/api", atividadeRoutes);
 app.use("/api", equipeRoutes);
