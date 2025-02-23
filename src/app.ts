5674de716ce2b9d45dac1efaa87037e583c348e4
Amaro Santos <j.amarorn@gmail.com>
2025-02-23 00:08:03 -0300

    Refactor CORS configuration and server setup

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,28 @@
 import express from "express";
+import cors from "cors";
 import vendedorRoutes from "./interfaces/routes/vendedorRoutes";
 import atividadeRoutes from "./interfaces/routes/atividadeRoutes";
 import equipeRoutes from "./interfaces/routes/equipeRoutes";
 import metaRoutes from "./interfaces/routes/metaRoutes";
-import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+// Use o middleware CORS
+app.use(cors({
+    origin: '*', // Permitir todos os domínios. Ajuste conforme necessário.
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Adicionando o endpoint de health check
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "UP", message: "Service is running smoothly" });
 });
 
-app.use(cors({
-    origin: '*',
-    //methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    //allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
 app.use("/api", vendedorRoutes);
 app.use("/api", atividadeRoutes);
 app.use("/api", equipeRoutes);
 app.use("/api", metaRoutes);
 
-export default app;``
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,7 @@
-import express from "express";
-import cors from "cors"; // Importe o pacote cors
+import app from "./app";
 import { MongoDB } from "./infrastructure/database/MongoDB";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Use o middleware CORS
-app.use(cors());
 
 MongoDB.conectar().then(() => {
     app.listen(PORT, () => {
